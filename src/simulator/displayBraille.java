c788097fd0d5071f8dd0fd2d0578f3a8a7a07542
Jon <Jon@DESKTOP-ANP7UD0>
2017-01-16 12:47:57 -0500

    added new file

---
+++ src/simulator/displayBraille.java
@@ -0,0 +1,16 @@
+package simulator;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class displayBraille {
+	
+	static void putBraille(String input, GridPane gridPane) {
+	
+		
+		for(int i=0; i<input.length(); i++) {
+			Label temp = new Label(Character.toString(input.charAt(i)));
+			gridPane.add(temp, i+3, 3);
+		}
+	}
+}
