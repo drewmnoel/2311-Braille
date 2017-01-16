23f9cbd9ff1c9a7d25a960b285b63fcfed64f869
Drew Noel <drewmnoel@gmail.com>
2017-01-16 12:40:21 -0500

    Add support for uppercase

--- src/simulator/BrailleTextTranslator.java
+++ src/simulator/BrailleTextTranslator.java
@@ -4,7 +4,12 @@ public class BrailleTextTranslator {
 	public static String translate(String input) {
 		StringBuilder result = new StringBuilder();
 		for (char c: input.toCharArray()) {
-			System.out.print(BrailleTextSymbol.ht.get(new String(c + "")));
+			String strC = new String(c + "");
+			if (strC.equals(strC.toUpperCase())) {
+				System.out.print(BrailleTextSymbol.ht.get("capital"));
+				strC = strC.toLowerCase();
+			}
+			System.out.print(BrailleTextSymbol.ht.get(strC));
 		}
 		return result.toString();
 	}
