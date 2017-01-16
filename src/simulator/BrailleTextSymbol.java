a64c1ec6158cc1d1381ebf1b35e18de080161be4
Drew Noel <drewmnoel@gmail.com>
2017-01-16 12:40:13 -0500

    Fix case to be lowercase

--- src/simulator/BrailleTextSymbol.java
+++ src/simulator/BrailleTextSymbol.java
@@ -6,32 +6,32 @@ public class BrailleTextSymbol {
 	public static final Hashtable<String,String> ht = new Hashtable<>();
 	
 	static {
-		ht.put("A", "⠁");
-		ht.put("B", "⠃");
-		ht.put("C", "⠉");
-		ht.put("D", "⠙");
-		ht.put("E", "⠑");
-		ht.put("F", "⠋");
-		ht.put("G", "⠛");
-		ht.put("H", "⠓");
-		ht.put("I", "⠊");
-		ht.put("J", "⠚");
-		ht.put("K", "⠅");
-		ht.put("L", "⠇");
-		ht.put("M", "⠍");
-		ht.put("N", "⠝");
-		ht.put("O", "⠕");
-		ht.put("P", "⠏");
-		ht.put("Q", "⠟");
-		ht.put("R", "⠗");
-		ht.put("S", "⠎");
-		ht.put("T", "⠞");
-		ht.put("U", "⠥");
-		ht.put("V", "⠧");
-		ht.put("W", "⠺");
-		ht.put("X", "⠭");
-		ht.put("Y", "⠽");
-		ht.put("Z", "⠵");
+		ht.put("a", "⠁");
+		ht.put("b", "⠃");
+		ht.put("c", "⠉");
+		ht.put("d", "⠙");
+		ht.put("e", "⠑");
+		ht.put("f", "⠋");
+		ht.put("g", "⠛");
+		ht.put("h", "⠓");
+		ht.put("i", "⠊");
+		ht.put("j", "⠚");
+		ht.put("k", "⠅");
+		ht.put("l", "⠇");
+		ht.put("m", "⠍");
+		ht.put("n", "⠝");
+		ht.put("o", "⠕");
+		ht.put("p", "⠏");
+		ht.put("q", "⠟");
+		ht.put("r", "⠗");
+		ht.put("s", "⠎");
+		ht.put("t", "⠞");
+		ht.put("u", "⠥");
+		ht.put("v", "⠧");
+		ht.put("w", "⠺");
+		ht.put("x", "⠭");
+		ht.put("y", "⠽");
+		ht.put("z", "⠵");
 		ht.put("0", "⠴");
 		ht.put("1", "⠂");
 		ht.put("2", "⠆");
@@ -42,5 +42,6 @@ public class BrailleTextSymbol {
 		ht.put("7", "⠶");
 		ht.put("8", "⠦");
 		ht.put("9", "⠔");
+		ht.put("capital", "⠠");
 	}
 }
