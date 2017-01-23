a6f7e7f656e6cbd545fc6ca26a8eb2a4e7fa843e
Jon <Jon@DESKTOP-ANP7UD0>
2017-01-23 13:03:11 -0500

    added simulator core to represent braille cells and buttons (to do)

---
+++ src/simulator/SimulatorCore.java
@@ -0,0 +1,50 @@
+package simulator;
+
+import java.util.ArrayList;
+
+public class SimulatorCore {
+	
+	private ArrayList<int[]> cellList;
+	private int buttons;
+	
+	//Constructor creating a simulator with numCells braille cells and numButton buttons
+	public SimulatorCore (int numCells, int numButton) throws Exception {
+		if (numCells >= 10 || numCells < 1) {
+			Exception error = new Exception("Enter a number of cells between 1 and 10");
+			throw error;
+		}
+		
+		int[] cell = {0, 0, 0, 0, 0, 0};
+		cellList = new ArrayList<int[]>(numCells);
+		
+		for (int i = 0; i<numCells; i++) {
+			cellList.add(cell);
+		}
+		
+		buttons = numButton;
+	}
+	
+	//Method to set a specific braille cell to raise/lower specified dots
+	public void setCell(int cellNumber, int[] dots) throws Exception {
+		if (cellNumber >= cellList.size() || cellNumber < 1) {
+			Exception error = new Exception("Enter a legal cell number");
+			throw error;
+		}
+		if (dots.length != 6) {
+			Exception error = new Exception("Must set value for 6 dots");
+			throw error;
+		}
+		cellList.set(cellNumber, dots);
+	}
+	
+	//method to set all braille cells to lowered
+	public void clearCells() {
+		int[] cell = {0, 0, 0, 0, 0, 0};
+		for (int i = 0; i<cellList.size(); i++) {
+			cellList.set(i, cell);
+		}
+	}
+
+	
+	
+}
