00ff6e579464d347a931c7e248c8558bab17a357
Drew Noel <drewmnoel@gmail.com>
2017-01-23 11:43:59 -0500

    Remove interface

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,28 +1,14 @@
 package simulator;
 
-import common.IBrailleSystem;
-
 /**
  * Represents a simulation model of the system.
  */
-public class Simulator implements IBrailleSystem {
+public class Simulator {
 
 	/**
 	 * Private constructor, only allowed to be used by itself.
 	 */
 	public Simulator() {
 		javafx.application.Application.launch(SimulatorDisplay.class);
-	}
-
-	@Override
-	public void setMessage(String msg) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String getResponse() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
