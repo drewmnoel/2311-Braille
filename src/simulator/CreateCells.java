ae4772ec53d1a1f681c31c898132932d587b99df
Dilshad Khatri <khatri.dilshad@gmail.com>
2017-01-30 13:12:17 -0500

    Made Application.css simpler Added empty cells in createCells Added TilePane by replacing gridPane

--- src/simulator/CreateCells.java
+++ src/simulator/CreateCells.java
@@ -1,7 +1,5 @@
 package simulator;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,6 +7,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -27,87 +26,67 @@ public class CreateCells extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Scanner stdin = new Scanner(System.in);
-
-		// Get the word to show
-		System.out.print("Word to display? ");
-		String brailleText = stdin.nextLine();
-
-		// Get the number of buttons
-		System.out.print("Number of buttons? ");
-		int numButtons = stdin.nextInt();
-
+		int numBrailleCells = 9;
 		primaryStage.setTitle("Enjoy your game.");
 
-		SimulatorCore simCore = new SimulatorCore(brailleText.length(), numButtons);
+		SimulatorCore simCore = new SimulatorCore(numBrailleCells, 5);
 
-		// Set the strings
-		int[][] nCells = BrailleTextTranslator.translate(brailleText);
-		for (int i = 0; i < brailleText.length(); i++) {
-			simCore.setCell(i, nCells[i]);
-		}
+		// Set example cells
+		simCore.setCell(0, new int[] {1, 1, 1, 0, 1, 0, 1, 1});
+		simCore.setCell(1, new int[] {0, 0, 1, 0, 0, 0, 1, 0});
+		simCore.setCell(2, new int[] {0, 1, 1, 1, 0, 0, 0, 0});
 
-		GridPane grid = new GridPane();
+
+		TilePane grid = new TilePane();
 		// grid.getStyleClass().add("game-grid");
+		
 
-		// TODO: Make programatically generated
-		Button yes = new Button("YES");
-		Button no = new Button("NO");
-
-		// Constrain each column to be BRAILLE_BOX_SIDE wide
-		for (int i = 0; i < brailleText.length() * 3; i++) {
-			ColumnConstraints column = new ColumnConstraints(BRAILLE_BOX_SIDE);
-			grid.getColumnConstraints().add(column);
-		}
-
-		// Constrain each row to be BRAILLE_BOX_SIDE tall
-		for (int i = 0; i < SCREEN_ROWS; i++) {
-			RowConstraints row = new RowConstraints(BRAILLE_BOX_SIDE);
-			grid.getRowConstraints().add(row);
-		}
+		
 
 		// Layout each of the cells
 		for (int k = 0; k < simCore.numOfCells(); k++) {
 			// Create a running index
 			int[] cellArray = simCore.cellAt(k);
+			GridPane secondary = new GridPane();
+			// Constrain each column to be BRAILLE_BOX_SIDE wide
+			
 
+			// Constrain each row to be BRAILLE_BOX_SIDE tall
+			
 			int runningIndex = 0;
 			for (int i = 0; i < BRAILLE_WIDTH; i++) {
 				for (int j = 0; j < BRAILLE_HEIGHT; j++) {
 					Pane pane = new Pane();
+					pane.setPrefSize(BRAILLE_BOX_SIDE, BRAILLE_BOX_SIDE);
 					pane.getChildren().add(BrailleCircle.getCircle(cellArray, runningIndex));
 					pane.getStyleClass().add("game-grid-cell");
-					if (i == 0) {
-						pane.getStyleClass().add("first-column");
-					}
-					if (j == 0) {
-						pane.getStyleClass().add("first-row");
-					}
-					grid.add(pane, i + (3 * k), j);
+				
+					secondary.add(pane, i + (3 * k), j);
 
 					runningIndex++;
 				}
 			}
+			grid.getChildren().add(secondary);
+			grid.setHgap(20);
+			grid.setVgap(20);
 		}
 
 		// Add two buttons
-		grid.add(yes, 0, BRAILLE_HEIGHT);
+		/*grid.add(yes, 0, BRAILLE_HEIGHT);
 		grid.add(no, 3, BRAILLE_HEIGHT);
-
+*/
 		// Scene width will be approx 3 braille boxes wide per braille cell
 		// (except only 2 on last cell)
-		int sceneWidth = 3 * BRAILLE_BOX_SIDE * brailleText.length() - BRAILLE_BOX_SIDE;
+		int sceneWidth = 3 * BRAILLE_BOX_SIDE * numBrailleCells - BRAILLE_BOX_SIDE;
 
 		// Scene height will be approx the height of 1 cell plus one more box
 		int sceneHeight = ((BRAILLE_HEIGHT + 1) * BRAILLE_BOX_SIDE);
 
-		Scene scene = new Scene(grid, sceneWidth, sceneHeight, Color.WHITE);
+		Scene scene = new Scene(grid);
 		scene.getStylesheets().add("application.css");
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
-		stdin.close();
 	}
 
 	public static void main(String[] args) {
