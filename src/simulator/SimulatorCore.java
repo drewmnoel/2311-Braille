34824dc023278c2374dd697408fa1da7d69a6d6e
jonnotung <jonno.tung@gmail.com>
2017-02-04 14:45:42 -0500

    Added javadoc comments for all public methods, except for ready()

--- src/simulator/SimulatorCore.java
+++ src/simulator/SimulatorCore.java
@@ -82,15 +82,17 @@ public class SimulatorCore {
 		ready = true;
 	}
 	/**
-	 * Use this method to set the pin states for a specific braille cell in the simulator
+	 * Use this method to set the pin states for a specific braille cell in the simulator.
 	 * Braille cells are 0 indexed.
-	 * The pins on a braille cell are represented using an integer array of length 8
-	 * Values of '0' indicate lowered pins, values of '1' indicate raised pins
-	 * Pins are a 0 indexed version of the standard numbering of dots in a braille cell
+	 * The pins on a braille cell are represented using an integer array of length 8.
+	 * Values of '0' indicate lowered pins, values of '1' indicate raised pins.
+	 * Pins are numbered using a 0 indexed version of the standard numbering of dots in a braille cell.
 	 * 
 	 * @param cellNumber The index of the braille cell to be set
-	 * @param dots A length 8 integer array
-	 * @throws SimulatorException
+	 * @param dots A length 8 integer array containing only 0's and 1's
+	 * @throws SimulatorException if cellNumber is not a valid braille cell index
+	 * @throws SimulatorException if dots is not length 8
+	 * @throws SimulatorException if dots contains an integer other than 0 or 1
 	 */
 	// Method to set a specific braille cell to raise/lower specified dots
 	public void setCell(int cellNumber, int[] dots) throws SimulatorException {
@@ -112,7 +114,10 @@ public class SimulatorCore {
 		cellObserver.set(cellNumber, dots);
 	}
 
-	// method to set all braille cells to lowered
+	
+	/**
+	 * This method to set all the pins in all braille cells in the simulator to 'lowered' position
+	 */
 	public void clearCells() {
 
 		for (int i = 0; i < cellObserver.size(); i++) {
@@ -121,7 +126,17 @@ public class SimulatorCore {
 		}
 	}
 
-	// method to return the braille cell at index i
+	/**
+	 * Use this method to retrieve the state of pins in a single braille cell in the simulator.
+	 * Braille cells 0 indexed.
+	 * This method returns a length 8 integer array .
+	 * Pins are numbered using a 0 indexed version of the standard numbering of dots in a braille cell.
+	 * Values of '0' indicate lowered pins, values of '1' indicate raised pins.
+	 * 
+	 * @param i Index of the braille cell whose state is being returned
+	 * @return A length 8 integer array of 0's and 1's
+	 * @throws SimulatorException if i is not a valid braille cell index
+	 */
 	public int[] cellAt(int i) throws SimulatorException {
 		if (i >= cellObserver.size() || i < 0) {
 			SimulatorException error = new SimulatorException("Enter a legal cell number");
@@ -130,7 +145,14 @@ public class SimulatorCore {
 		return cellObserver.get(i);
 	}
 
-	// method to return an ArrayList copy of the list of braille cells
+	
+	/**
+	 * Use this method to retrieve the state of all braille cells in the simulator.
+	 * Braille cells are represented as length 8 integer arrays, with '0' indicating lowered and '1' indicating raised pins
+	 * The ArrayList returned by this method is ordered the same as the braille cells in the simulator
+	 * 
+	 * @return an Arraylist<int[]> containing int[] representations of all the braille cells in the simulator
+	 */
 	public ArrayList<int[]> allCells() {
 		ArrayList<int[]> brailleCells = new ArrayList<int[]>();
 		for (int i = 0; i < cellObserver.size(); i++) {
@@ -139,15 +161,28 @@ public class SimulatorCore {
 		return brailleCells;
 	}
 
-	//Returns the number of braille cells
+	/**
+	 * Use this method to retrieve the number of braille cells present in the simulator
+	 * 
+	 * @return an int between 1 and 10 of how many braille cells are present in the simulator
+	 */
 	public int numOfCells() {
 		return cellObserver.size();
 	}
 
+	/**
+	 * Use this method to retrieve the number of buttons present in the simulator
+	 * 
+	 * @return in int between 1 and 5 of how many buttons are present in the simulator
+	 */
 	public int numOfButtons() {
 		return buttons;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public static boolean ready() {
 		return ready;
 	}
