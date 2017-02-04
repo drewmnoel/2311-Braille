9bc18cc90f96baac1366bd96cbfd077cea241eb8
jonnotung <jonno.tung@gmail.com>
2017-02-04 12:14:16 -0500

    started writing javadoc comments for SimulatorCore

--- src/simulator/SimulatorCore.java
+++ src/simulator/SimulatorCore.java
@@ -5,6 +5,17 @@ import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * 
+ * <h1>SimulatorCore</h1>
+ * 
+ * <p>The SimulatorCore class provides a simulation of the function of a braille player hardware. It supports the creation of a simulation of hardware with 1 to 10 braille cells and 1 to 5 buttons.</p>
+ * 
+ * 
+ * @author Dilshad Khatri, Alvis Koshy, Drew Noel, Jonathan Tung
+ *
+ */
+
 public class SimulatorCore {
 
 	protected static ObservableList<int[]> cellObserver = null;
