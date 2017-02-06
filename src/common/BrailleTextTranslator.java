21311591260187fd6bda8d39b6c001ad1c1b9d70
Jon <Jon@DESKTOP-ANP7UD0>
2017-02-06 12:38:36 -0500

    Added a JUnit test case for BrailleTextTranslator Added javadoc comments for BrailleTextTranslator

--- src/common/BrailleTextTranslator.java
+++ src/common/BrailleTextTranslator.java
@@ -7,10 +7,15 @@ package common;
 public class BrailleTextTranslator {
 
 	/**
-	 * Convert string input to array of Braille cells
-	 * @param input Text to convert into Braille cells
+	 * Convert string input to array of Braille cells.
+	 * Only supports translation of lower case alphabet characters and numbers 0 to 9.
+	 * For all other characters use the setCell() method in the SimulatorCore class to set
+	 * the cell pins directly. 
+	 * 
+	 * @param input String of text to convert into Braille cells. Must only contain lower case letters or numbers 0-9.  
 	 * @return Braille result stored in an array of cells
 	 */
+	
 	public static int[][] translate(String input) {
 		int[][] result = new int[input.length()][];
 		int i = 0;
--- src/tests/BrailleTextTranslatorTest.java
+++ src/tests/BrailleTextTranslatorTest.java
@@ -5,19 +5,54 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import common.BrailleTextTranslator;
 import common.BrailleTextSymbol;
 import simulator.SimulatorCore;
 import simulator.SimulatorException;
+import java.util.Arrays;
 
 public class BrailleTextTranslatorTest {
-
+	String testString;
+	int[][] expectedResult = {
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{1, 1, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 1, 0, 0, 0},
+			{1, 0, 0, 0, 1, 1, 0, 0},
+			{1, 0, 0, 0, 0, 1, 0, 0},
+			{1, 1, 0, 0, 1, 0, 0, 0},
+			{1, 1, 0, 0, 1, 1, 0, 0},
+			{1, 1, 0, 0, 0, 1, 0, 0},
+			{0, 1, 0, 0, 1, 0, 0, 0},
+			{0, 1, 0, 0, 1, 1, 0, 0},
+			{1, 0, 1, 0, 0, 0, 0, 0},
+			{1, 1, 1, 0, 0, 0, 0, 0},
+			{1, 0, 1, 0, 1, 0, 0, 0},
+			{1, 0, 1, 0, 1, 1, 0, 0},
+			{1, 0, 1, 0, 0, 1, 0, 0},
+			{1, 1, 1, 0, 1, 0, 0, 0},
+			{1, 1, 1, 0, 1, 1, 0, 0},
+			{1, 1, 1, 0, 0, 1, 0, 0},
+			{0, 1, 1, 0, 1, 0, 0, 0},
+			{0, 1, 1, 0, 1, 1, 0, 0},
+			{1, 0, 1, 0, 0, 0, 1, 0},
+			{1, 1, 1, 0, 0, 0, 1, 0},
+			{0, 1, 1, 0, 1, 1, 1, 0},
+			{1, 0, 1, 0, 1, 0, 1, 0},
+			{1, 0, 1, 0, 1, 1, 1, 0},
+			{1, 0, 1, 0, 0, 1, 1, 0}
+	};
 	@Before
 	public void setUp() throws Exception {
+		testString = "abcdefghijklmnopqrstuvwxyz";
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTranslateString() {
+		int[][]translateResult = BrailleTextTranslator.translate(testString);
+		for(int i = 0; i<26; i++) {
+			assertTrue(Arrays.equals(translateResult[i], expectedResult[i]));
+		}
+		
 	}
-
 }
