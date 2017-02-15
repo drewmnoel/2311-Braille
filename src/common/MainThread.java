76f0cb66c65ac16e1914768d26c7fa6be26c0fe0
Drew Noel <drewmnoel@gmail.com>
2017-02-15 10:38:35 -0500

    Update demo for TTS and AudioPlayer

--- src/common/MainThread.java
+++ src/common/MainThread.java
@@ -1,6 +1,8 @@
 package common;
 
-import player.TTSException;
+import player.AudioPlayer;
+import player.Event;
+import player.FileParser;
 import player.TextToSpeech;
 import simulator.SimulatorCore;
 
@@ -18,40 +20,41 @@ public class MainThread implements Runnable {
 	public void run() {
 		API api = new API();
 		TextToSpeech tts;
+		FileParser fp = new FileParser();
+		AudioPlayer ap = new AudioPlayer();
 
+		// If anything goes wrong, bail out all at once.
 		try {
 			tts = new TextToSpeech();
-		} catch (TTSException e) {
-			System.err.println(e.getStackTrace());
-			return;
-		}
+			fp.setFileTarget("test.txt");
 
-		// Wait for the core information to be prepopulated to avoid null
-		// pointers
-		while (!SimulatorCore.ready()) {
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
+			// Parse the scenario file
+			for (Event e : fp.parseFile()) {
+				if (e.isTTS()) {
+					tts.say(e.getEventDetails());
+				} else if (e.isAudioPlay()) {
+					ap.playFile(e.getEventDetails());
+				}
 			}
-		}
 
-		// Core is ready, demo setting the display to several strings
-		try {
-			tts.say("Setting text to test");
+			// Wait for the core information to be prepopulated to avoid null
+			// pointers
+			while (!SimulatorCore.ready()) {
+				Thread.sleep(1);
+			}
+
+			// Core is ready, demo setting the display to several strings
 			api.setText("test");
 			Thread.sleep(1500);
 
-			tts.say("Setting text to hello");
 			api.setText("hello");
 			api.setButtons(4);
 			Thread.sleep(1500);
 
-			tts.say("Setting text to bye");
 			api.setText("bye");
 
 			tts.shutdown();
-		} catch (Exception e) {
-		}
+		} catch (Exception e) {}
 	}
 
 }
