14af95900a7fc1440b1848cad6cd23467a052a81
Jon <Jon@DESKTOP-ANP7UD0>
2017-02-27 13:13:57 -0500

    Started work on test GUI for player

---
+++ src/common/PlayerTestGUI.java
@@ -0,0 +1,28 @@
+package common;
+
+import javafx.application.Application;
+import javafx.collections.ListChangeListener;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+/*
+ * Test GUI for control of the player
+ * 
+ */
+
+public class PlayerTestGUI extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+}
