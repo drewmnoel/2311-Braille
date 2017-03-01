39c20b1519e6ff31eb7c184045820eebd9eaa8da
Jon <Jon@DESKTOP-ANP7UD0>
2017-03-01 12:46:26 -0500

    Added more event types to Event class

--- src/player/Event.java
+++ src/player/Event.java
@@ -1,7 +1,8 @@
 package player;
 
 /**
- * Abstract representation of an event in the scenario.
+ * Abstract representation of an event in the scenario. 
+ * Events are created by FileParser parsing an input file
  *
  * @author Dilshad Khatri, Alvis Koshy, Drew Noel, Jonathan Tung
  * @version 1.0
@@ -9,8 +10,17 @@ package player;
  *
  */
 public class Event {
+	//Attributes to identify kind of event
+	//Text to speech
 	private static final long TTS = 0;
 	private static final long AUDIO_PLAY = 1;
+	private static final long BRAILLE = 2;
+	//wait for push of a single specific button only
+	private static final long SINGLE_BUTTON_PUSH = 3;
+	//wait one out of multiple possible buttons being pushed
+	private static final long CHOICE_OF_BUTTONS = 4;
+	//Jump to a specified event instead of continuing sequentially
+	private static final long JUMP = 5;
 	private long type;
 	private String eventDetails;
 
@@ -59,7 +69,56 @@ public class Event {
 	public boolean isAudioPlay() {
 		return this.type == Event.AUDIO_PLAY;
 	}
-
+	
+	/**
+	 * Set the event to be Set Braille
+	 * 
+	 * @param wordToSet
+	 * 			String to represent on braille cells
+	 */
+	public void setBraille(String wordToSet) {
+		this.type = Event.BRAILLE;
+		this.eventDetails = wordToSet;
+	}
+	
+	/**
+	 * Check if the event is a Set Braille
+	 * 
+	 * @return True only if event is a Set Braille
+	 */
+	public boolean isSetBraille() {
+		return this.type == Event.BRAILLE;
+	}
+	
+	/**
+	 * Set the event to be single button push
+	 * 
+	 * @param button 
+	 * 			Button number to expect a push from
+	 */
+	public void setSingleButton(String button) {
+		this.type = Event.SINGLE_BUTTON_PUSH;
+		this.eventDetails = button;
+	}
+	
+	/**
+	 * Check if the event is a single button
+	 * 
+	 * @return True only if event is a single buttonn
+	 */
+	public boolean isSingleButton() {
+		return this.type == Event.SINGLE_BUTTON_PUSH;
+	}
+	
+	/**
+	 * Returns a long representing the type of this event
+	 * 
+	 * @return 0 if TTS, 1 if audio play, 2 if set braille cell, 3 if wait for button push, 4 if jump event
+	 */
+	public long eventType() {
+		return this.type;
+	}
+	
 	/**
 	 * Get the generic details parameter
 	 *
