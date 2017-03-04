f0424ec78eb5732512885127fd76f06497954a02
Drew Noel <drewmnoel@gmail.com>
2017-03-04 12:15:24 -0500

    Implement jump button listener

--- src/common/MainThread.java
+++ src/common/MainThread.java
@@ -82,6 +82,30 @@ public class MainThread implements Runnable {
 			//jump to an event non-sequentially
 			tempSteps = executeJump(thisEvent);
 		}
+		else if (thisEvent.isButton()) {
+			// Determine how many buttons we have to figure out
+			int numButtons = thisEvent.getEventDetails().split(" ").length;
+
+			// Create a shared object to track which button gets clicked
+			SharedObject sharedObject = new SharedObject();
+			sharedObject.setId(-1);
+
+			// Create the listeners and give them access to the shared object
+			for (int i = 0; i < numButtons; i++) {
+				sim.getButton(i).addActionListener(new JumpButtonListener(sharedObject, i));
+			}
+
+			// Wait for a button to change the shared value.
+			while (sharedObject.getId() == -1) {
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {}
+			}
+
+			// Shared Object now contains the number of the button that was pressed.
+			return Integer.parseInt(thisEvent.getEventDetails().split(" ")[sharedObject.getId()]);
+
+		}
 
 		return tempSteps;
 	}
