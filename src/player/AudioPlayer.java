c3be03641efdfd0f9003f24be8e5e75f2b7f18ae
Drew Noel <drewmnoel@gmail.com>
2017-03-06 15:03:24 -0500

    Update JavaDoc for AudioPlayer

--- src/player/AudioPlayer.java
+++ src/player/AudioPlayer.java
@@ -11,7 +11,7 @@ import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
- * Basic implementation of an Audio Player to play select filetypes
+ * Basic implementation of an Audio Player to play select filetypes. Currently only WAV files are supported.
  *
  * @author Dilshad Khatri, Alvis Koshy, Drew Noel, Jonathan Tung
  * @version 1.0
@@ -19,26 +19,19 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 public class AudioPlayer {
 	/**
-	 * Create an audio player with default settings
-	 */
-
-
-	public AudioPlayer() {
-	}
-
-	/**
-	 * Play a given audio file.
+	 * Play a given audio file. Attempts to open the file (given in either
+	 * relative or absolute path) and play it through the default audio sink.
+	 * Supports only WAV files. Method will begin and wait until audio file is
+	 * completed before returning. Since this will tie the current thread until
+	 * the file is done playing, using another thread may be desired.
 	 *
 	 * @param fileName
 	 *            Path to the file to be played
-	 *
-	 * @param filename
-	 *            Path to file which should be played. Accepts both absolute and
-	 *            relative paths.
 	 * @throws PlayerException
-	 * @throws IOException
-	 * @throws UnsupportedAudioFileException
-	 * @throws LineUnavailableException
+	 *             If there is any issue with playing the file. This exception
+	 *             wrappers three types of problems: wrong file format (e.g. not
+	 *             WAV), inaccessible audio sink, and file I/O errors. The
+	 *             exception is chained with the source of the problem.
 	 */
 	public void playFile(String fileName) throws PlayerException {
 		File file = new File(fileName);
@@ -50,7 +43,7 @@ public class AudioPlayer {
 			audioClip.open(format);
 			audioClip.start();
 
-			while(stream.available() > 0) {
+			while (stream.available() > 0) {
 				int len = stream.read(buffer);
 				audioClip.write(buffer, 0, len);
 			}
@@ -59,7 +52,8 @@ public class AudioPlayer {
 			audioClip.close();
 
 		} catch (UnsupportedAudioFileException e) {
-			throw new PlayerException("An unsupported audio file was given. Only WAV files are currently supported.", e);
+			throw new PlayerException("An unsupported audio file was given. Only WAV files are currently supported",
+					e);
 		} catch (LineUnavailableException e) {
 			throw new PlayerException("Output audio sink could not be accessed", e);
 		} catch (IOException e) {
