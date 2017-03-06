79a372b6341d874e8f0a86ce2eb56785f62aa8fc
jonnotung <jonno.tung@gmail.com>
2017-03-06 18:28:32 -0500

    changed test file

--- src/common/MainThread.java
+++ src/common/MainThread.java
@@ -22,7 +22,7 @@ public class MainThread implements Runnable {
 		try {
 			//File parser to parse events from input file
 			FileParser fp = new FileParser();
-			fp.setFileTarget("test.txt");
+			fp.setFileTarget("testexception.txt");
 			//List of events parsed from input file. parseFile return is implemented as an array list
 			List<Event> eventList = fp.parseFile();
 			//Checking for possible errors with respect to cells and buttons
