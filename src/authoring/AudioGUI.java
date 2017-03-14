1b03a085d9de72f42caf47b201375ed70bb8652e
Dilshad Khatri <Khatri.dilshad@gmail.com>
2017-03-14 01:43:32 -0400

    Trying to put GUI with audioRecorder

--- src/authoring/AudioGUI.java
+++ src/authoring/AudioGUI.java
@@ -1,41 +1,69 @@
 package authoring;
 
-import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+public class AudioGUI extends JFrame implements ActionListener{
+	private JButton btnStart = new JButton("Start");
+	private JButton btnStop = new JButton("Stop");
+	AudioThread audioThread;
+		
+	public void AudioGUI(){
+		audioThread = new AudioThread();
+		setTitle("AudioRecorder");
+		
+		getContentPane().setLayout(new FlowLayout());
+		getContentPane().add(btnStart);
+		getContentPane().add(btnStop);
+		
+		btnStart.addActionListener(this);
+		btnStop.addActionListener(this);
+	}
 
-public class AudioGUI {
-	private final JFrame audioFrame = new JFrame();
-	private final JPanel audioPanel = new JPanel();
-	private FlowLayout layout= new FlowLayout();
-	private int height = 100;
-	private int width = 300;
 	
-	public void setHeight(int height){
-		this.height = height;
+	public static void main(String[] args)
+	{
+		AudioGUI frame = new AudioGUI();
+		frame.setTitle("two buttons");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(100, 100);
+		frame.setVisible(true);
 	}
-	
-	public void setWidth(int width){
-		this.width = width;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnStart) {
+			System.out.println("Button start");
+			Thread thread = new Thread(audioThread);
+			thread.start();
+		} else if (e.getSource() == btnStop) {
+			System.out.println("Button start");
+			audioThread.stop();
+		}
+		
 	}
+}
+class AudioThread implements Runnable{
+	AudioRecorder recorder = new AudioRecorder();
+	String fileName = "testAudio.wav";
+	boolean check = false;
 	
-	public void displayGUI(){
-		audioFrame.setSize(width, height);
-		audioPanel.setLayout(layout);
-		audioPanel.add(this.createButtons("Start"));
-		audioPanel.add(this.createButtons("ReadFile"));
-		audioPanel.add(this.createButtons("Stop"));
-		audioFrame.add(audioPanel);
-		audioFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		audioFrame.setVisible(true);
+	public void run() {
+		while(check)
+		{
+			try{
+				Thread.sleep(10);
+			}catch(InterruptedException ex){
+				ex.printStackTrace();
+			}
+			recorder.recordAudio();
+		}
+		
 	}
-	
-	private JButton createButtons(String btnName){
-		JButton btn = new JButton(btnName);
-		return btn;
+	public void stop(){
+		recorder.stopRecording();
+		check = false;
 	}
 	
 }
