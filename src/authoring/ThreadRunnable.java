9073107b42f57f4eae2039e30f5439b292cd7817
Drew Noel <drewmnoel@gmail.com>
2017-03-21 21:18:44 -0400

    Quick comment cleanup

--- src/authoring/ThreadRunnable.java
+++ src/authoring/ThreadRunnable.java
@@ -19,8 +19,9 @@ public class ThreadRunnable extends Thread {
 	 *
 	 * @param fileName The path (local/absolute) to the output file to save
 	 */
-	public ThreadRunnable(String fileName) {
-		// Define the audio format to be WAVE audio, Microsoft PCM, 16 bit, stereo 44100 Hz
+	public ThreadRunnable() {
+		// Define the audio format to be WAVE audio, Microsoft PCM, 16 bit,
+		// stereo 44100 Hz
 		AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0F, 16, 2, 4, 44100.0F, false);
 		DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat);
 
@@ -41,7 +42,8 @@ public class ThreadRunnable extends Thread {
 		// Begin listening on the recording line
 		line.start();
 
-		// Now call Thread's start (which triggers ThreadRunnable's run() for us)
+		// Now call Thread's start (which triggers ThreadRunnable's run() for
+		// us)
 		super.start();
 	}
 
@@ -55,7 +57,8 @@ public class ThreadRunnable extends Thread {
 	 * Close the line, which will terminate the recorder and therefore stop the thread.
 	 */
 	public void stopRecording() {
-		// Stop the line and then close it. The recorder will drain the line itself.
+		// Stop the line and then close it. The recorder will drain the line
+		// itself.
 		line.stop();
 		line.close();
 	}
