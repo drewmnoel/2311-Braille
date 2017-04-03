22253d8249567e57d15edc5f6e0b51bece9583c4
Drew Noel <drewmnoel@gmail.com>
2017-04-03 11:43:32 -0400

    Fix export filename setting

--- src/listeners/ExportListener.java
+++ src/listeners/ExportListener.java
@@ -2,7 +2,15 @@ package listeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.apache.commons.io.FilenameUtils;
 
 import authoring.GUI;
 import commands.PlayerCommand;
@@ -32,6 +40,26 @@ public class ExportListener implements ActionListener {
 
 		// sb now contains the export file contents
 		// TODO: Save to file rather than printing out
-		System.out.print(sb.toString());
+		JFileChooser save = new JFileChooser();
+
+		FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("text files (*.txt)", "txt");
+		save.addChoosableFileFilter(txtFilter);
+		save.setFileFilter(txtFilter);
+
+		save.showSaveDialog(null);
+
+		// Get the file and fix the extension if it's wrong
+		File file = save.getSelectedFile();
+		if (!FilenameUtils.getExtension(file.getName()).equalsIgnoreCase("txt")) {
+			file = new File(file.toString() + ".txt");
+		}
+
+		try {
+			FileWriter fw = new FileWriter(file);
+			fw.write(sb.toString());
+			fw.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
