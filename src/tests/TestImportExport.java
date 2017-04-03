8d487289017516b65a3c38b65bb772a4206d74b7
Drew Noel <drewmnoel@gmail.com>
2017-04-03 13:27:10 -0400

    Add basic export test

---
+++ src/tests/TestImportExport.java
@@ -0,0 +1,32 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import commands.CellCharCommand;
+import commands.PlayerCommand;
+import commands.RepeatCommand;
+import listeners.ExportListener;
+
+public class TestImportExport {
+
+	@Test
+	public void testExportSimple() {
+		List<PlayerCommand> list = new ArrayList<>();
+
+		list.add(new CellCharCommand("a"));
+		list.add(new RepeatCommand("Test repeat"));
+
+		ExportListener el = new ExportListener(null);
+
+		String result = el.parseCommands(list);
+		String expectedResult = "/~disp-cell-char:a\n/~repeat\nTest repeat\n/~endrepeat";
+
+		// Trim newlines from the end, ensure they're equal still
+		assertEquals(result.trim(), expectedResult.trim());
+	}
+}
