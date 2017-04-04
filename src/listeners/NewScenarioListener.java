d48404212f96317c0ac1ca2a1604a5f73f9b3152
agkoshy <alviskoshy@gmail.com>
2017-04-04 16:17:08 -0400

    Documentation for the NewScenarioListener

--- src/listeners/NewScenarioListener.java
+++ src/listeners/NewScenarioListener.java
@@ -1,6 +1,5 @@
 package listeners;
 
-
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,87 +12,115 @@ import javax.swing.JTextField;
 
 import authoring.GUI;
 import authoring.RightPanel;
+
 /**
  * NewScenarioListener class
  * 
- * The NewScenarioListener class is a listener that responds to a button 
- * @author Alvis
+ * This class is used as an action listener whenever the "New Scenario" button
+ * is clicked. It serves as a way to initialize the application. Allowing the
+ * user to set the bounds of the scenario they are creating such as the number
+ * of Cells and Buttons and can define what the title of the scenario should be.
+ * 
+ * @author Dilshad Khatri, Alvis Koshy, Drew Noel, Jonathan Tung
+ * @version 1.0
+ * @since 4/3/2017
  *
  */
 public class NewScenarioListener implements ActionListener {
 
+	// A GUI object to utilize the panels
 	private GUI gui;
-	private JLabel cellLabel = new JLabel("Cell: " );
-	private JLabel btnLabel = new JLabel("Button: " );
-	private JLabel titleLabel = new JLabel("Title: " );
+
+	// Labels and Textfields to be used to set their respective fields
+	private JLabel cellLabel = new JLabel("Cell: ");
+	private JLabel btnLabel = new JLabel("Button: ");
+	private JLabel titleLabel = new JLabel("Title: ");
 	private JTextField celltxt = new JTextField(5);
-    private JTextField btntxt = new JTextField(5);
+	private JTextField btntxt = new JTextField(5);
 	private JTextField titletxt = new JTextField(5);
 	private JPanel myPanel = new JPanel(new GridLayout(0, 1, 5, 5));
-	
-	public NewScenarioListener(GUI gui)
-	{
+
+	public NewScenarioListener(GUI gui) {
 		this.gui = gui;
 	}
-	
+
+	/**
+	 * This method builds a pane that contains 3 fields that take input from a
+	 * user to set parameters in the Settings Panel fields
+	 */
 	private void scenarioBuilder() {
+		// call to the clearItem() method that clears out all events in the
+		// scenario
 		gui.getLeftPanel().clearItem();
 
-		 myPanel.add(cellLabel);
-	     myPanel.add(celltxt); 
+		// Adding the labels and textfields to create a layout for pane
+		myPanel.add(cellLabel);
+		myPanel.add(celltxt);
+		myPanel.add(btnLabel);
+		myPanel.add(btntxt);
+		myPanel.add(titleLabel);
+		myPanel.add(titletxt);
 
-	     myPanel.add(btnLabel);
-	     myPanel.add(btntxt); 
-	     myPanel.add(titleLabel);
-	     myPanel.add(titletxt);
+		// Creates a Confirmation Pane with an "OK" and "CANCEL" option
+		int result = JOptionPane.showConfirmDialog(null, myPanel, "Enter the Scenario parameters",
+				JOptionPane.OK_CANCEL_OPTION);
 
+		if (result == JOptionPane.OK_OPTION) {
+			// Checks to see if the fields are empty if user hits "OK"
+			if (celltxt.getText().isEmpty() || btntxt.getText().isEmpty() || titletxt.getText().isEmpty()) {
+				// Shows an error pane if the user does hit "OK" with empty
+				// fields
+				JOptionPane.showMessageDialog(null, "One or more fields are empty!", "Missing fields!",
+						JOptionPane.ERROR_MESSAGE);
+			}
 
-	     int result = JOptionPane.showConfirmDialog(null, myPanel, 
-	              "Enter the Scenario parameters", JOptionPane.OK_CANCEL_OPTION);
-	     
-	     if (result == JOptionPane.OK_OPTION) {
-	    	   if(celltxt.getText().isEmpty() || btntxt.getText().isEmpty() || titletxt.getText().isEmpty())
-	  	     {
-	  	    	JOptionPane.showMessageDialog(null,
-	  	    			    "One or more fields are empty!",
-	  	    			    "Missing fields!",
-	  	    			    JOptionPane.ERROR_MESSAGE);
-	  	     }
-	        gui.getSettingsPanel().setCellField(celltxt.getText());
-	        gui.getSettingsPanel().setButtonFieldText(btntxt.getText());
-	        gui.getSettingsPanel().setTitleField(titletxt.getText());
-	        gui.getRightPanel().setStart(true);
-	        gui.getRightPanel().setNew(true);
-	        gui.getRightPanel().setExport(true);
-	        gui.getRightPanel().setReadFile(true);
-	     }
+			// Takes the text that was input by the user in the fields and sets
+			// them in the SettingsPanel fields
+			gui.getSettingsPanel().setCellField(celltxt.getText());
+			gui.getSettingsPanel().setButtonFieldText(btntxt.getText());
+			gui.getSettingsPanel().setTitleField(titletxt.getText());
+
+			// Enables buttons to true only after the user initializes
+			gui.getRightPanel().setStart(true);
+			gui.getRightPanel().setNew(true);
+			gui.getRightPanel().setExport(true);
+			gui.getRightPanel().setReadFile(true);
+		}
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(gui.getLeftPanel().elementCheck()) 
-		{
+		// A check to see if the Scenario pane is empty
+		if (gui.getLeftPanel().elementCheck()) {
 
+			// Clears the text fields in the setting panel
 			gui.getSettingsPanel().setCellField("");
 			gui.getSettingsPanel().setButtonFieldText("");
 			gui.getSettingsPanel().setTitleField("");
+
+			// Clears the text in the fields when "New Scenario" is pressed
 			celltxt.setText("");
 			btntxt.setText("");
 			titletxt.setText("");
+
+			// Calls the scenarioBuilder() method
 			scenarioBuilder();
 
-		 } else {
-			 int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to start a new Scenario?"
-			 		+ " All current changes will be lost", "Warning!", 
-			 		JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
-		     if (result == JOptionPane.YES_OPTION) {
+		} else {
+
+			// If the scenario isn't empty, a warning message is displayed
+			int result = JOptionPane.showConfirmDialog(null,
+					"Are you sure you want to start a new Scenario?" + " All current changes will be lost", "Warning!",
+					JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
 				gui.getSettingsPanel().setCellField("");
 				gui.getSettingsPanel().setButtonFieldText("");
 				gui.getSettingsPanel().setTitleField("");
 				celltxt.setText("");
 				btntxt.setText("");
-				titletxt.setText("");				
-		    	scenarioBuilder();
-		     }
-		 }   		
+				titletxt.setText("");
+				scenarioBuilder();
+			}
+		}
 	}
 }
