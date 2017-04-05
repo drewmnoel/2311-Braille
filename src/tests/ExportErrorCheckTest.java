c608b8308c9f5a7d85baeb3fc8b572341db0f94a
jonnotung <jonno.tung@gmail.com>
2017-04-04 21:44:44 -0400

    Added a class with basic error checking utilities for a scenario before saving

---
+++ src/tests/ExportErrorCheckTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.Assert.*;
+import commands.*;
+import authoring.ExportErrorCheck;
+
+import org.junit.Test;
+
+public class ExportErrorCheckTest {
+
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
